0bb13c1dcb6516c81b41bdaa2f3d59ac39910991
PrompipatJa <prompipat18348@gmail.com>
2025-09-05 13:35:45 +0700

    try some component with shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,59 @@
 <script>
 	import Card from '../component/card.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Pagination from '$lib/components/ui/pagination/index.js';
+	import * as Table from '$lib/components/ui/table/index.js';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Card />
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
 
-<Button>Click me</Button>
+<div class="bg-gray-200 w-full h-screen flex flex-col items-center justify-center">
+	<Button class="w-32 h-12 text-xl cursor-pointer">Crawl</Button>
+	<div class="w-3/4 my-8 bg-white p-4 rounded-lg shadow-md">
+		<Table.Root>
+			<Table.Caption>A list of your recent invoices.</Table.Caption>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[100px]">Invoice</Table.Head>
+					<Table.Head>Status</Table.Head>
+					<Table.Head>Method</Table.Head>
+					<Table.Head class="text-right">Amount</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell class="font-medium">INV001</Table.Cell>
+					<Table.Cell>Paid</Table.Cell>
+					<Table.Cell>Credit Card</Table.Cell>
+					<Table.Cell class="text-right">$250.00</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+	</div>
+	<Pagination.Root count={100} perPage={10} class="bg-white p-4 rounded-lg shadow-md w-1/4">
+		{#snippet children({ pages, currentPage })}
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton />
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === 'ellipsis'}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link {page} isActive={currentPage === page.value}>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton />
+				</Pagination.Item>
+			</Pagination.Content>
+		{/snippet}
+	</Pagination.Root>
+</div>
