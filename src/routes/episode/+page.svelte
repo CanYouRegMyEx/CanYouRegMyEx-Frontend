112cd8f2c3d21e88ac749685399712c9855f40f9
PrompipatJa <prompipat18348@gmail.com>
2025-09-06 12:09:12 +0700

    feat: update styles and add episode page content

---
+++ src/routes/episode/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="w-full h-screen px-10 py-10 bg-white text-black font-geist gap-4">
+	<h1>Roller Coaster Murder Case</h1>
+	<div class="flex items-end gap-2">
+		<h3>Episode 1 / Season 1</h3>
+		<h4>January 8, 1996</h4>
+	</div>
+	<p>
+		Roller Coaster Murder Case (ジェットコースター殺人事件 Jetto Kōsutā Satsujin Jiken?) is the
+		first episode of Detective Conan anime. The anime begins in the middle of a case where a murder
+		was committed during a party at a private mansion, introducing the high-school student
+		detective Shinichi Kudo who came to solve it. In the next day, he comes with Ran Mouri to
+		the Tropical Land where another murder would be committed in a roller-coaster. Being the first
+		episode, most of the main characters are introduced throughout the episode. The introduction of
+		the characters Ayumi, Mitsuhiko, and Genta are anime-original, as their first manga appearance
+		would be only in volume 2.
+	</p>
+</div>
