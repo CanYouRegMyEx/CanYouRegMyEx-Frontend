0ba119127ce944a7308dd14c2e42365f989cca90
pppoh <2ndPipatpong987123@gmail.com>
2025-09-17 20:13:16 +0700

    delete redirect for people

--- src/routes/episode/+page.svelte
+++ src/routes/episode/+page.svelte
@@ -132,7 +132,7 @@
 		<!-- Card list -->
 		<div class="flex flex-wrap gap-4">
 			{#each episode.main_characters || [] as character}
-				<a href="/character/{encodeURIComponent(character.name_eng || 'Unknown')}" class="no-underline">
+				<a href="{character.character_url}" class="no-underline">
 					<Card.Root
 						class="w-[300px] h-fit opacity-100 flex flex-col justify-center items-center gap-4 p-6 rounded-[10px] border border-solid border-gray-200 cursor-pointer hover:shadow-lg hover:scale-105 transition-all duration-300 hover:border-blue-300"
 					>
@@ -244,7 +244,7 @@
 
 				<div class="flex flex-wrap gap-4">
 					{#each episode.side_characters || [] as person}
-						<a href="/character/{encodeURIComponent(person.name_eng || 'Unknown')}" class="no-underline">
+						<a href="" class="no-underline">
 							<Card.Root
 								class="w-[300px] h-fit opacity-100 flex flex-col justify-center items-center gap-4 p-6 rounded-[10px] border border-solid border-gray-200 cursor-pointer hover:shadow-lg hover:scale-105 transition-all duration-300 hover:border-blue-300"
 							>
